209367443fab829e8f61bd5955a57db13ce34743
Dhanushchandra <Dhanushbharath9@gmail.com>
2023-02-28 23:49:41 +0530

    initial commit with all basic components and pages

---
+++ src/components/Home/Hero.js
@@ -0,0 +1,21 @@
+import './Hero.css';
+
+const Hero = () => {
+    return(
+        <>
+        <header className='hero'>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center hero-content">
+        <h1>REVA RESEARCH CONCLAVE(RRC)-2023</h1>
+        <p>Dec 16 & 17 - R&D Cell in association with School of Computing and Information Technology, REVA UNIVERSITY, Bangalore</p>
+      </div>
+    </div>
+  </div>
+</header>
+
+        </>
+    )
+}
+
+export default Hero;
